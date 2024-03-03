6a8ef79cfaad458662b5758474f11d062e04408b
ömer uysal <omeremreu361@gmail.com>
2024-03-03 17:05:45 +0300

    Ömer Emre Uysal

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿namespace odev_1;
+
+class Program
+{
+    public static int Alan { get; private set; }
+
+    static void Main(string[] args)
+    {
+    /*
+     Console.WriteLine("1.sayıyı girin:");
+     int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+     Console.WriteLine("2.sayıyı girin:");
+     int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("3.sayıyı girin:");
+      int sayi3 = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine("4.sayıyı girin:");
+      int sayi4 = Convert.ToInt32(Console.ReadLine());
+
+       int toplam = (sayi1+sayi2+sayi3+sayi4) / 4;
+       Console.WriteLine("Bu sayıların ortalaması:  "+ toplam);
+       */
+    /*
+    Console.WriteLine("x giriniz:");
+    int x = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("y giriniz:");
+    int y = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("z giriniz:");
+    int z = Convert.ToInt32(Console.ReadLine());
+
+    int toplam = (x+y)*z;
+    int toplam1 = x*y + y*z;
+
+    Console.WriteLine($"({x}+{y})*{z} = {toplam} ve {x}*{y} + {y}*{z} = {toplam1}");
+    */
+    
+    /*
+    Console.WriteLine("Yaşınızı Giriniz :");
+    int yas = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine($"{yas} yaşından daha yaşlı gözüküyorsun");
+    */
+     
+    /*
+    Console.WriteLine("Yarıçap giriniz :");
+    int yarıcap = Convert.ToInt32(Console.ReadLine());
+    
+    double pi = 3.14;
+    double yuzey = 4 * pi * yarıcap * yarıcap;
+    double hacım = 4/3 * pi * yarıcap * yarıcap * yarıcap;
+
+    Console.WriteLine($"yüzey : {yuzey}");
+    Console.WriteLine($"hacim : {hacım}");
+    */
+    
+    /*
+    console.WriteLine("1.sayıyı giriniz : ");
+    int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("2.sayıyı giriniz : ");
+    int sayi2 = Convert.ToInt32(Console.ReadLine())
+
+    int toplama = sayi1 + sayi2
+    int cıkarma = sayi1 - sayi2
+
+    Console.WriteLine($"{sayi1} + {sayi2} = {toplama}");
+    Console.WriteLine($"{sayi1} - {sayi2} = {cıkarma}");
+    Console.WriteLine($"{sayi1} * {sayi2} = {carpma}");
+    Console.WriteLine($"{sayi1} / {sayi2} = {bolme}");
+    Console.WriteLine($"{sayi1} mod {sayi2} = {mod}");
+    */
+    /*
+    Console.WriteLine("Santigrat girin :");
+    int san = Convert.ToInt32(Console.ReadLine());
+
+    int kelvin = san + 273;
+    int fahrenayt = san * 18/10 +32;
+
+    Console.WriteLine($"Kelvin = {kelvin}");
+    Console.WriteLine($"Fahrenheit = {fahrenayt}");
+    */
+    /*
+    Console.WriteLine("Genişlik girin :");
+    int gen = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Yükseklik girin :");
+    int yük = Convert.ToInt32(Console.ReadLine());
+
+    int cevre = gen + yük *2;
+    int alan = gen * yük;
+    int pisagor = gen * gen + yük * yük;
+    double kare = Math.Sqrt(pisagor);
+    
+    Console.WriteLine($"Çevre = {cevre}");
+    Console.WriteLine($"Alan = {alan}");
+    Console.WriteLine($"Köşegen = {kare}");
+    */
+    }
+
+
+}
